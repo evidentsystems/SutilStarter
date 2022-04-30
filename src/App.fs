6ef3bee590378272d95c7347f9c182dd1b3e2484
Mike Shearer <shearerbeard@gmail.com>
2022-04-30 13:57:59 -0600

    add fantomas and fmt pass

--- src/App.fs
+++ src/App.fs
@@ -6,55 +6,36 @@ open Sutil.Attr
 
 importAll "./index.css"
 
-type Model =
-    { count: int }
+type Model = { count: int }
 
 type Msg =
     | Increment
     | Decrement
 
-let init (): Model * Cmd<Msg> =
+let init () : Model * Cmd<Msg> =
     let model = { count = 0 }
     let cmd = Cmd.none
     model, cmd
 
-let update (msg: Msg) (model: Model): Model * Cmd<Msg> =
+let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match msg with
-        | Increment ->
-            { model with count = model.count + 1 }, Cmd.none
-        | Decrement ->
-            { model with count = model.count - 1 }, Cmd.none
+    | Increment -> { model with count = model.count + 1 }, Cmd.none
+    | Decrement -> { model with count = model.count - 1 }, Cmd.none
 
 let getCounter m = m.count
 
-let app() =
+let app () =
     let model, dispatch = () |> Store.makeElmish init update ignore
-    Html.div [
-        Html.h1 [
-            class' "text-2xl font-bold underline"
-            Html.text "Hello world!"
-        ]
-        Html.div [
-            Html.p [
-                Bind.fragment (model |> Store.map getCounter) <| fun n ->
-                    Html.text $"Counter = {n}"
-            ]
-        ]
-        Html.div [
-            Html.button [
-                class' "btn"
-                onClick (fun _ -> dispatch Increment) []
-                Html.text "(+)"
-            ]
-        ]
-        Html.div [
-            Html.button [
-                class' "btn"
-                onClick (fun _ -> dispatch Decrement) []
-                Html.text "(-)"
-            ]
-        ]
-    ]
 
-app() |> Program.mountElement "sutil-app"
+    Html.div [ Html.h1 [ class' "text-2xl font-bold underline"
+                         Html.text "Hello world!" ]
+               Html.div [ Html.p [ Bind.fragment (model |> Store.map getCounter)
+                                   <| fun n -> Html.text $"Counter = {n}" ] ]
+               Html.div [ Html.button [ class' "btn"
+                                        onClick (fun _ -> dispatch Increment) []
+                                        Html.text "(+)" ] ]
+               Html.div [ Html.button [ class' "btn"
+                                        onClick (fun _ -> dispatch Decrement) []
+                                        Html.text "(-)" ] ] ]
 
+app () |> Program.mountElement "sutil-app"
